d280b2e580eb7b867b7d3f2a81d067db81f1c6e2
vonxing <284934875@qq.com>
2021-01-21 17:12:30 +0800

    提交

--- jd_gyec.js
+++ jd_gyec.js
@@ -31,8 +31,10 @@ const $ = new Env('工业品爱消除');
 const notify = $.isNode() ? require('./sendNotify') : '';
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 let inviteCodes = [
-  '840266@2585219@2586018@1556311@2583822@2585256@756497@1234613',
-  '840266@2585219@2586018@1556311@2583822@2585256@756497@1234613',
+  '2873557@2873561@2873566',
+  '2873557@2873561@2873566',
+  '2873557@2873561@2873566',
+  '2873557@2873561@2873566',
 ]
 const ACT_ID = 'A_112790_R_4_D_20201209'
 let exchangeName = $.isNode() ? (process.env.EXCHANGE_GYEC ? process.env.EXCHANGE_GYEC : '1888京豆') : ($.getdata('JDGYEC') ? $.getdata('JDGYEC') : '1888京豆')
